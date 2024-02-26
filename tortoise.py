28b443849923c0caea8dbc481e795f154eda3527
keerthanadurgam <160122154+keerthanadurgam@users.noreply.github.com>
2024-02-26 16:10:05 +0530

    tortoise is created (#2)
    Co-authored-by: Admin <admin@Admins-MacBook-Pro-5.local>

---
+++ tortoise.py
@@ -0,0 +1,15 @@
+class Animal:
+    def eat(self):
+         print("I can Eat")
+
+    def sleep(self):
+         print("I can Sleep")
+
+    def sound(self):
+         print("I can't Sound")
+
+    def run(self):
+         print("I can't Run")
+
+    def swim(self):
+         print("I can Swim")
